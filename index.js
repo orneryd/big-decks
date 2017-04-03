f5a27ea29468e413aa25a7fb22512b6634821015
Tim Sweet <tim.sweet@gm.com>
2017-04-02 18:43:01 -0700

    index and gitignore

---
+++ index.js
@@ -0,0 +1,9 @@
+
+import * as cheerio from 'node_modules/cheerio';
+
+const $ = cheerio.load('<h2 class="title">Hello world</h2>')
+
+$('h2.title').text('Hello there!')
+$('h2').addClass('welcome')
+
+console.log($.html())
